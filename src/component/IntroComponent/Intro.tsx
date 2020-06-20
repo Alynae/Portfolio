cf7cead23ac02758cd8316540283c4c3abd31edd
jeremylb <jeremylebricquer@gmail.com>
2020-06-20 13:49:41 +0200

    mouse icon

--- src/component/IntroComponent/Intro.tsx
+++ src/component/IntroComponent/Intro.tsx
@@ -8,29 +8,7 @@ import { Carousel } from "react-responsive-carousel";
 
 export default function Intro() {
   return (
-    <section >
-      
-      {/*       <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="hello ">
-              <h1>Bonjour, je suis Jérémy Le bricquer</h1>
-              <h3>
-                <span style={{ textShadow: "black 0px 0px 0px", opacity: "1" }}>
-                  Dev junior - Front &amp;Back{" "}
-                </span>
-              </h3>
-            </div>
-
-            <a href="/resume">
-              <div className="mouse-icon">
-                <div className="wheel"></div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div> */}
-
+    <section>
       <Carousel
         showArrows={false}
         showThumbs={false}
@@ -41,8 +19,7 @@ export default function Intro() {
         infiniteLoop={true}
         interval={2500}
         swipeable={true}
-        >
-          
+      >
         <div>
           <img src={banner1} alt="banner" />
         </div>
@@ -52,8 +29,31 @@ export default function Intro() {
         <div>
           <img src={banner3} alt="banner" />
         </div>
-      </Carousel>
 
+    
+      </Carousel>
+      <div className="container" id="overlay" >
+          <div className="row">
+            <div className="col-md-12">
+              <div className="hello ">
+                <h1>Bonjour, je suis Jérémy Le bricquer</h1>
+                <h3>
+                  <span
+                    style={{ textShadow: "black 0px 0px 0px", opacity: "1" }}
+                  >
+                    Dev junior - Front &amp;Back{" "}
+                  </span>
+                </h3>
+              </div>
+
+              <a href="/resume">
+                <div className="mouse-icon">
+                  <div className="wheel"></div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
     </section>
   );
 }
