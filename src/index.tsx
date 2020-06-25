3f61452107c295fc06a9316ef0e1af59ae260859
jeremylb <jeremylebricquer@gmail.com>
2020-06-25 13:40:26 +0200

    implement recaptcha google v3

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { GoogleReCaptchaProvider,GoogleReCaptcha } from 'react-google-recaptcha-v3';
+import logger from "./logger";
 
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.setItem('debug', 'Logger:*');
+if (process.env.NODE_ENV !== "production") {
+  localStorage.setItem("debug", "Logger:*");
 }
 
+const CaptchaKey = process.env.REACT_APP_RECAPTCHA_KEY;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GoogleReCaptchaProvider
+      reCaptchaKey={CaptchaKey}
+      language="fr"
+    >
+       <GoogleReCaptcha onVerify={token => logger.info('token',token)} />
+      <App />
+    </GoogleReCaptchaProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
