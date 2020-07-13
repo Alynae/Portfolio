8b56689bc8f6af968cda067afb78d9eda2569dfe
jeremylb <jeremylebricquer@gmail.com>
2020-07-13 17:51:36 +0200

    underline css

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import {NavLink} from 'react-router-dom'
 
 export default function Navigation() {
   return (
-    <Navbar bg="light" expand="lg" fixed="top">
-      <Navbar.Brand href="/">Jérémy Le bricquer</Navbar.Brand>
+    <Navbar bg="light" expand="lg" fixed="top" >
+      <Navbar.Brand as={NavLink} exact to="/">Jérémy Le bricquer</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end ">
-        <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/resume">Resume</Nav.Link>
-          <Nav.Link href="/formation">Formation</Nav.Link>
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav >
+          <Nav.Link  as={NavLink}exact to="/" >Home</Nav.Link>
+          <Nav.Link as={NavLink} exact to="/resume" >Resume</Nav.Link>
+          <Nav.Link  as={NavLink} exact to="/formation"  >Formation</Nav.Link>
+          <Nav.Link  as={NavLink} exact to="/portfolio" >Portfolio</Nav.Link>
+          <Nav.Link  as={NavLink} exact to="/contact" >Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
